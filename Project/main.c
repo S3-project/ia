1700b77ad43201e2add8d1ad99f0d8ebb6a5e521
Adrien Pingard <adrienpingard@pop-os.localdomain>
2020-11-14 19:05:37 +0100

    2test ajouter

--- Project/main.c
+++ Project/main.c
@@ -6,9 +6,15 @@
 
 #include "main.h"
 #include "other/Bitmap/bitmap.h"
+#include "pre-processing/Removing_Colors/rmcolors.h"
 #include "characters_detection/characters_detection.h"
 
-
+void delay(int a)
+{
+	int b = 0;
+	for (int i = 0; i < a*10000000; i++)
+		b++;
+}
 
 
 
@@ -18,10 +24,55 @@ void test1()
 }
 
 
+void TestLoadImage(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error Syntax\nSyntax: %s %s {imagePath}\n", argv[0], argv[1]);
+		return;
+	} 
+
+
+	BMPIMAGE *img;
+	printf("---- Test load image ----\n");
+	printf("image path: '%s'\nLoading image...", argv[2]);
+	img = LoadBitmap(argv[2]);
+	printf("OK\n");
+	printf("Printing image data...\n\n");
+	PrintBitmap(img);
+
+	FreeBitmap(img);
+}
 
 
 
+void TestToBlackWhite(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		printf("Error Syntax\nSyntax: %s %s {imagePath} {savePath}\n", argv[0], argv[1]);
+		return;
+	}
 
+	BMPIMAGE *img;
+	printf("---- Test save Black and White image ----\n");
+	printf("image path: '%s'\nLoading image...", argv[2]);
+	img = LoadBitmap(argv[2]);
+	printf("OK\n");
+	printf("Converting to black and white..");
+	BMPIMAGE *gray = ToGrayBitmap(img);
+	BMPIMAGE *black = ToBlackWhite(gray);
+	printf("OK\n");
+
+
+	printf("\nSaving the black and white image..\n");
+	SaveBitmap(black, argv[3]);
+	printf("The image have been saved at '%s'.\n", argv[3]);
+
+	FreeBitmap(img);
+	FreeBitmap(gray);
+	FreeBitmap(black);
+}
 
 
 
@@ -56,16 +107,16 @@ int	isEqual(char* s1, char *s2)
 
 int main(int argc, char **argv)
 {
-	if (argc != 2)
+	if (argc < 2)
 	{
 		printf("Error Syntax\nSyntax: %s {str}\n", argv[0]);
 		return 1;
 	}
 
-	if (isEqual(argv[1], "test1"))
-		test1();
-	else if(isEqual(argv[1], "test2"))
-		test1();
+	if (isEqual(argv[1], "loadimage"))
+		TestLoadImage(argc, argv);
+	else if(isEqual(argv[1], "saveblackwhite"))
+		TestToBlackWhite(argc, argv);
 	else
 	{
 		printf("Test not Found: %s\n", argv[1]);
