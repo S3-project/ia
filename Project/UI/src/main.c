b8414df9f6617fda469bf279a55eea1388de985e
pierre.litoux <pierre.litoux@gmail.com>
2020-12-10 18:16:35 +0100

    voila

---
+++ Project/UI/src/main.c
@@ -0,0 +1,41 @@
+#include <"gtk/gtk.h">
+
+GtkWidget *g_lbl_hello;
+GtkWidget *g_lbl_count;
+
+int main(int argc, char *argv[])
+{
+    GtkBuilder      *builder; 
+    GtkWidget       *window;
+
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "UI.glade", NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_1"));
+    gtk_builder_connect_signals(builder, NULL);
+    
+    // get pointers to the labels
+    g_lbl_hello = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_hello"));
+    g_lbl_count = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_count"));
+
+    g_object_unref(builder);
+
+    gtk_widget_show(window);                
+    gtk_main();
+
+    return 0;
+}
+
+// called when button is clicked
+void on_btn_execute_clicked()
+{
+	printf("on_btn_clicked()");
+}
+
+// called when window is closed
+void on_window_1_destroy()
+{
+    gtk_main_quit();
+}
