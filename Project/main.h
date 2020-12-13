94eab7468328b664deddca88f50a8d2b4015850f
Thimot <thimot.veyre@gmail.com>
2020-12-13 14:53:22 +0100

    modif

--- Project/main.h
+++ Project/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char * LaunchOCR(char *filename, char *neuralNetworkFileName, double rotation);
+char * LaunchOCR(char *filename, char *neuralNetworkFileName, double rotation, int denoising);
 void TrainIA(char *dataBaseImagesFilename, char *dataBaseLabelsFilename, char *neuralNetworkFileName, int iteration, double *progression);
 
 #endif //MAIN_H
